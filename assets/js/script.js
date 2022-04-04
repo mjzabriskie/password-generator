8070e13aa1f8e84a07d4983843af3574480901e7
mjzabriskie <99937908+mjzabriskie@users.noreply.github.com>
2022-04-03 23:56:32 -0600

    added functionality to check that at least one character from each selected group is included in the password.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
 // Assignment code here
 var generatePassword = function () {
   var nbrChar = null;
-  while (nbrChar < 8 || nbrChar > 128 || isNaN(nbrChar)) { //checks if nbrChar is a number from 8 to 128
-    nbrChar = parseInt(prompt("Number of characters for the generated password (min 8, max 128)", "8"));
+  while (nbrChar < 8 || nbrChar > 128 || isNaN(nbrChar)) {
+    //checks if nbrChar is a number from 8 to 128
+    nbrChar = parseInt(
+      prompt(
+        "Number of characters for the generated password (min 8, max 128)",
+        "8"
+      )
+    );
   }
   var lowerCase = false;
   var upperCase = false;
   var numeric = false;
   var special = false;
 
-  while (!lowerCase && !upperCase && !numeric && !special) { //Storing which character sets the user wishes to use.
+  while (!lowerCase && !upperCase && !numeric && !special) {
+    //Storing which character sets the user wishes to use.
     lowerCase = window.confirm('Select "OK" to include lower-case letters.');
     upperCase = window.confirm('Select "OK" to include upper-case letters.');
     numeric = window.confirm('Select "OK" to include numbers.');
@@ -18,40 +25,108 @@ var generatePassword = function () {
       window.alert("You must select at least one character type.");
     }
   }
+
   var lowerCaseSet = "abcdefghijklmnopqrstuvwxyz";
   var upperCaseSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numericSet = "1234567890";
-  var specialSet = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~" + '"';
+  var specialSet = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~" + '"';
   var combinedSet = "";
 
-  if (lowerCase) {//combines the selected character sets into one string
-      combinedSet += lowerCaseSet;
+  if (lowerCase) {
+    //combines the selected character sets into one string
+    combinedSet += lowerCaseSet;
   }
 
   if (upperCase) {
-      combinedSet += upperCaseSet;
+    combinedSet += upperCaseSet;
   }
 
   if (numeric) {
-      combinedSet += numericSet;
+    combinedSet += numericSet;
   }
 
   if (special) {
-      combinedSet += specialSet;
+    combinedSet += specialSet;
   }
 
   //loops for number of characters user entered,
   //uses Math.random() to select random character from combinedSet.
   var fullPassword = "";
 
-  for (var i = 0; i < nbrChar; i++) {
-    var charSelect = Math.floor(Math.random() * combinedSet.length) + 1;
-    var charItem = combinedSet.charAt(charSelect - 1);
+  var setPassword = function () {
+    if (fullPassword) {
+      fullPassword = "";
+    }
 
-    fullPassword += charItem;
-  }
+    for (var i = 0; i < nbrChar; i++) {
+      var charSelect = Math.floor(Math.random() * combinedSet.length);
+      var charItem = combinedSet.charAt(charSelect);
+
+      fullPassword += charItem;
+    }
+    if (lowerCase) {//if lowerCaseSet was selected, checks that password contains at least one char from the set.
+      var lowerArray = lowerCaseSet.split("");
+
+      for (i = 0; i < lowerArray.length; i++) {
+        var atLeastOne = fullPassword.match(lowerArray[i]);
+
+        if (atLeastOne) {
+          break;
+        }
+      }
+      if (!atLeastOne) {
+        setPassword();
+      }
+    }
+    if (upperCase) {//if upperCaseSet was selected, checks that password contains at least one char from the set.
+      var upperArray = upperCaseSet.split("");
+
+      for (i = 0; i < upperArray.length; i++) {
+        var atLeastOne = fullPassword.match(upperArray[i]);
+
+        if (atLeastOne) {
+          break;
+        }
+      }
+      if (!atLeastOne) {
+        setPassword();
+      }
+    }
+    if (numeric) {
+      //if numericSet was selected, checks that password contains at least one char from the set.
+      var numericArray = numericSet.split("");
+
+      for (i = 0; i < numericArray.length; i++) {
+        var atLeastOne = fullPassword.match(numericArray[i]);
+
+        if (atLeastOne) {
+          break;
+        }
+      }
+      if (!atLeastOne) {
+        setPassword();
+      }
+    }
+    if (special) {
+      //if specialSet was selected, checks that password contains at least one char from the set.
+      var specialArray = specialSet.split("");
+
+      for (i = 0; i < specialArray.length; i++) {
+        var atLeastOne = fullPassword.match(/[^\w\s]/); //Special characters gave me trouble, even in a string.
+                                                        //Used this regex code to match anything that's not an alphanumeric.
+        if (atLeastOne) {
+          break;
+        }
+      }
+      if (!atLeastOne) {
+        setPassword();
+      }
+    }
+    return fullPassword;
+  };
+  setPassword();
   return fullPassword;
-}
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -65,7 +140,7 @@ function writePassword() {
   passwordText.value = password;
 }
 
-function copyPassword() {
+function copyPassword() { //Added function to make password retrieval easier
   var getPassword = document.querySelector("#password");
   getPassword.select();
   getPassword.setSelectionRange(0, 99999);
